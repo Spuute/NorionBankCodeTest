8f9d912a01958d0e4e7f8c16d09454b8560af56b
Patric Bergkvist <spuute@gmail.com>
2024-04-13 15:25:00 +0200

    add holiday date helper class

---
+++ Application/Services/Dates/HolidayDateHelper.cs
@@ -0,0 +1,38 @@
+namespace Application.Services.Dates;
+
+public static class HolidayDateHelper
+{
+    public static bool IsWeekend(DateTime date)
+    {
+        return date.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday;
+    }
+
+    public static DateTime GetPreviousWorkDay(DateTime currentDay)
+    {
+        return currentDay.AddDays(-1);
+    }
+    
+    public static DateTime CalculateMidsummer(int year)
+    {
+        var dateTime = new DateTime(year, 6, 19);
+        for (var i = 0; i < 7; i++)
+        {
+            if (dateTime.AddDays(i).DayOfWeek == DayOfWeek.Friday)
+                return dateTime.AddDays(i);
+        }
+
+        // TODO: Create custom exception.
+        throw new ArgumentOutOfRangeException("Something went wrong when calculating midsummer.");
+    }
+    
+    public static bool IsChargeableDayBeforeHolidays(DateTime currentDay)
+    {
+        var dayBeforeMidsummer = GetPreviousWorkDay(CalculateMidsummer(currentDay.Year));
+        var dayBeforeChristmas = new DateTime(currentDay.Year, 12, 23);
+        var dayBeforeNewYears = new DateTime(currentDay.Year, 12, 30);
+        
+        return currentDay.Date == dayBeforeMidsummer || 
+               currentDay.Date == dayBeforeChristmas ||
+               currentDay.Date == dayBeforeNewYears;
+    }
+}
