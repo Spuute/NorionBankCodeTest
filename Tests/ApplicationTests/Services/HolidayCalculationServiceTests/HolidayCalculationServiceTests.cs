4a485ef8e2a0d7cdb0126e36c0b32639b46502e0
Patric Bergkvist <spuute@gmail.com>
2024-04-14 10:36:14 +0200

    refactor tests

--- Tests/ApplicationTests/Services/HolidayCalculationServiceTests/HolidayCalculationServiceTests.cs
+++ Tests/ApplicationTests/Services/HolidayCalculationServiceTests/HolidayCalculationServiceTests.cs
@@ -105,10 +105,11 @@ public class HolidayCalculationServiceTests
     }
 
     [TestMethod]
-    public void IsTollFreeDate_Christmas_ShouldReturnTrue()
+    [DynamicData(nameof(TollFreeDates2024))]
+    public void IsTollFreeDate_2024SpecificDates_ShouldReturnTrue(int year, int month, int day)
     {
         // Arrange
-        var christmas = new DateTime(2024, 12, 24);
+        var christmas = new DateTime(year, month, day);
         
         // Act
         var result = _holidayCalculationService.IsTollFreeDate(christmas);
@@ -118,15 +119,58 @@ public class HolidayCalculationServiceTests
     }
 
     [TestMethod]
-    public void IsTollFreeDate_DayBeforeChristmas_ShouldReturnFalse()
+    [DynamicData(nameof(SpecificNoneTollFreeDatesBeforeHolidays))]
+    public void IsTollFreeDate_SpecificDaysBeforeHolidays_ShouldReturnFalse(int year, int month, int day)
     {
         // Arrange
-        var dayBeforeChristmas = new DateTime(2024, 12, 23);
+        var date = new DateTime(year, month, day);
         
         // Act
-        var result = _holidayCalculationService.IsTollFreeDate(dayBeforeChristmas);
+        var result = _holidayCalculationService.IsTollFreeDate(date);
 
         // Assert
         Assert.IsFalse(result);
+    }
+    
+    
+
+    public static IEnumerable<object[]> TollFreeDates2024
+    {
+        get
+        {
+            return new[]
+            {
+                [2024, 01, 01],
+                [2024, 01, 05],
+                [2024, 03, 28],
+                [2024, 03, 29],
+                [2024, 04, 01],
+                [2024, 04, 30],
+                [2024, 05, 01],
+                [2024, 05, 08],
+                [2024, 05, 09],
+                [2024, 06, 05],
+                [2024, 06, 06],
+                [2024, 06, 21],
+                [2024, 11, 01],
+                [2024, 12, 24],
+                [2024, 12, 25],
+                [2024, 12, 26],
+                new object[] { 2024, 12, 31 }
+            };
+        }
+    }
+    
+    public static IEnumerable<object[]> SpecificNoneTollFreeDatesBeforeHolidays
+    {
+        get
+        {
+            return new[]
+            {
+                [2024, 06, 20],
+                [2024, 12, 23],
+                new object[] { 2024, 12, 30 }
+            };
+        }
     }
 }
