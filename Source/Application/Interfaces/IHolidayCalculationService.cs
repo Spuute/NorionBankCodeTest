ed12bd1b0f62e2ce95b21b91c5cc6820ca98be70
Patric Bergkvist <spuute@gmail.com>
2024-04-14 10:36:38 +0200

    add new method to interface

--- Source/Application/Interfaces/IHolidayCalculationService.cs
+++ Source/Application/Interfaces/IHolidayCalculationService.cs
@@ -3,5 +3,6 @@ namespace Application.Interfaces;
 public interface IHolidayCalculationService
 {
     bool IsWeekend(DateTime date);
+    bool IsJuly(DateTime date);
     bool IsTollFreeDate(DateTime date);
 }
